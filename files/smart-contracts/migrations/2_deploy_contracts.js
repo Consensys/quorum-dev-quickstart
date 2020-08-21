c8372c3d61ffb6fead9c87a966524b1d21a57b30
Joshua Fernandes <joshua.fernandes@consensys.net>
2020-08-21 16:39:59 +1000

    remove smart contract folder

--- files/smart-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-
-//privateFor is an extra transaction parameter for the contract deployment ie its private for a specific account, identified by the given public key (member3)
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage, 42, {privateFor: ["k2zXEin4Ip/qBGlRkJejnGWdP9cjkK+DAvKNW31L2C8="]})
-};
