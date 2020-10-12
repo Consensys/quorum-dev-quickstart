ad5db0e4e9e1b52bd5b3005aed296f0379392b7e
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2020-10-12 18:51:38 +1300

    bring orchestrate image manifest into local repo for easier updates

--- src/service/orchestrate/index.ts
+++ src/service/orchestrate/index.ts
@@ -1,4 +1,4 @@
-import { createWriteStream, unlinkSync } from "fs";
+import { createWriteStream, unlinkSync, readFileSync } from "fs";
 import { resolve as resolvePath } from "path";
 import got, { HTTPError } from "got";
 import { promisify } from "util";
@@ -26,10 +26,11 @@ export async function installOrchestrateImages(): Promise<void> {
 
     const accessToken = token.token;
 
-    const spinner = new Spinner("Fetching manifest").start();
+    const manifest = _fetchManifest();
+
+    const spinner = new Spinner("Fetching images").start();
 
     try {
-        const manifest = await _fetchManifest(accessToken);
 
         const tmpDirDesc = tmp.dirSync({ prefix: "quorum-dev-quickstart" });
 
@@ -77,97 +78,76 @@ async function _fetchAuthToken(): Promise<ExtendedToken> {
     return token;
 }
 
-async function _fetchManifest(token: string): Promise<ImageManifest> {
-    const headers = {
-        Authorization: `Bearer ${token}`
-    };
-
-    const relayUrlBase = process.env.QUORUM_DEV_QUICKSTART_RELAY_URL ?
-        process.env.QUORUM_DEV_QUICKSTART_RELAY_URL :
-        "https://relay.quorum.consensys.net";
-
-    try {
-        const manifestUrl = `${relayUrlBase}/quorum-dev-quickstart/manifest`;
-        return await got(manifestUrl, {
-            headers
-        }).json<ImageManifest>();
-
-
-    } catch (err) {
-        if (err instanceof HTTPError) {
-            if (err.response.statusCode === 401) {
-                throw new AuthenticationError(
-                    `There was a problem authenticating your account. Please check your username and password and try again.`
-                );
-            }
-
-            if (err.response.statusCode === 403) {
-                const authZFailureHeaderName = "x-authorization-failure-reason";
-                const authZFailureReason = err.response.headers[authZFailureHeaderName];
-                if (authZFailureReason) {
-                    if (authZFailureReason === "trial-not-started") {
-                        throw new AuthorizationError(
-                            `Authenticated successfully, but no trial entitlement can be found for this account.`,
-                            authZFailureReason
-                        );
-                    } else if (authZFailureReason === "malformed-trial-expiration") {
-                        throw new AuthorizationError(
-                            `Authenticated successfully, but the trial entitlement for this account is malformed. If the problem persists, please file a GitHub issue.`,
-                            authZFailureReason
-                        );
-                    } else if (authZFailureReason === "trial-expired") {
-                        throw new AuthorizationError(
-                            "Authenticated successfully, but your trial period for Codefi Orchestrate has " +
-                            "expired. For other licensing options you can use the form on the following page to " +
-                            "discuss your options with a member of our sales team:\n" +
-                            "http://codefi.consensys.net/orchestrate-get-in-touch",
-                            authZFailureReason
-                        );
-                    } else {
-                        throw new AuthorizationError(
-                            `There was a permissions issue with your account. Please try again. If the problem persists, please file a GitHub issue.`,
-                            Array.isArray(authZFailureReason) ? authZFailureReason.join(", ") : authZFailureReason
-                        );
-                    }
-                }
-
-                throw new Error(`There was a permissions issue with your account. Please try again. If the problem persists, please file a GitHub issue.`);
-            }
-
-            if (err.response.statusCode === 404) {
-                throw new Error(
-                    `The image manifest cannot be found. ` +
-                    `This sometimes happens when the image is being updated. ` +
-                    `Please wait a minute and try again.`
-                );
-            }
-        }
-        throw err;
-    }
+function _fetchManifest(): ImageManifest {
+  const rawManifestJson = readFileSync(resolvePath(__dirname, "..", "..", "..", "orchestrate-image-manifest.json"), "utf-8");
+  const manifest: ImageManifest = JSON.parse(rawManifestJson) as ImageManifest;
+  return manifest;
 }
 
 async function _downloadImage(token: string, entry: ImageManifestEntry, tmpDir: string): Promise<string> {
-    const headers = {
-        Authorization: `Bearer ${token}`
-    };
+  const headers = {
+    Authorization: `Bearer ${token}`
+  };
 
-    try {
-        const requestStream = got.stream(entry.url, { headers });
+  try {
+    const requestStream = got.stream(entry.url, { headers });
 
-        const savePath = resolvePath(tmpDir, entry.fileName);
-        const outputStream = createWriteStream(savePath);
+    const savePath = resolvePath(tmpDir, entry.fileName);
+    const outputStream = createWriteStream(savePath);
 
-        await pipeline(requestStream, outputStream);
+    await pipeline(requestStream, outputStream);
 
-        return savePath;
-    } catch (err) {
-        if (err instanceof HTTPError) {
-            if (err.response.statusCode === 403) {
-                throw new Error(`There was a problem authenticating your account. Please try again.`);
-            }
+    return savePath;
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      if (err.response.statusCode === 401) {
+        throw new AuthenticationError(
+          `There was a problem authenticating your account. Please check your username and password and try again.`
+        );
+      }
+
+      if (err.response.statusCode === 403) {
+        const authZFailureHeaderName = "x-authorization-failure-reason";
+        const authZFailureReason = err.response.headers[authZFailureHeaderName];
+        if (authZFailureReason) {
+          if (authZFailureReason === "trial-not-started") {
+            throw new AuthorizationError(
+              `Authenticated successfully, but no trial entitlement can be found for this account.`,
+              authZFailureReason
+            );
+          } else if (authZFailureReason === "malformed-trial-expiration") {
+            throw new AuthorizationError(
+              `Authenticated successfully, but the trial entitlement for this account is malformed. If the problem persists, please file a GitHub issue.`,
+              authZFailureReason
+            );
+          } else if (authZFailureReason === "trial-expired") {
+            throw new AuthorizationError(
+              "Authenticated successfully, but your trial period for Codefi Orchestrate has " +
+              "expired. For other licensing options you can use the form on the following page to " +
+              "discuss your options with a member of our sales team:\n" +
+              "http://codefi.consensys.net/orchestrate-get-in-touch",
+              authZFailureReason
+            );
+          } else {
+            throw new AuthorizationError(
+              `There was a permissions issue with your account. Please try again. If the problem persists, please file a GitHub issue.`,
+              Array.isArray(authZFailureReason) ? authZFailureReason.join(", ") : authZFailureReason
+            );
+          }
         }
-        throw err;
+
+        throw new Error(`There was a permissions issue with your account. Please try again. If the problem persists, please file a GitHub issue.`);
+      }
+
+      if (err.response.statusCode === 404) {
+        throw new Error(
+          `The image cannot be found. This sometimes happens when the image is` +
+          ` being updated. Please wait a minute and try again.`
+        );
+      }
     }
+    throw err;
+  }
 }
 
 if (require.main === module) {
