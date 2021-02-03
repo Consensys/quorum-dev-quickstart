94cd30c9e583e03228b1c6bf135ce8b3f14b05ec
ggarri <ggarri@gmail.com>
2021-02-03 10:54:40 +0100

    fix network restart using Orchestrate

--- templates/orchestrate/network.sh
+++ templates/orchestrate/network.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+. ./network/.env
+
 pushd ./network
 
 if [ $1 == "up" ]; then
-  if [ -f "./quorumDevQuickstart.lock" ]; then
+  if [ -f "$LOCK_FILE" ]; then
     . resume.sh
   else
     . run.sh
   fi
 elif [ $1 == "down" ]; then
 . stop.sh
+rm $LOCK_FILE
 fi
 
 popd
