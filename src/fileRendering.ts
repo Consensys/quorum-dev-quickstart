088b514db9d40b9ed4ed8e2c27d2538dbd45b410
Joshua Fernandes <joshua.fernandes@consensys.net>
2021-03-18 06:49:58 +1000

    updates to rendering images (#42)

--- src/fileRendering.ts
+++ src/fileRendering.ts
@@ -1,4 +1,5 @@
 import { renderString } from "nunjucks";
+import { isBinaryFileSync } from "isbinaryfile";
 import { resolve as resolvePath, join as joinPath, dirname } from "path";
 import fs from "fs";
 import os from "os";
@@ -17,14 +18,21 @@ export function copyFilesDir(filesBasePath: string, context: NetworkContext): vo
         const outputPath = resolvePath(context.outputPath, filePath);
         const outputDirname = dirname(outputPath);
 
-        const mode = fs.statSync(resolvePath(filesBasePath, filePath)).mode;
-        const fileSrc = fs.readFileSync(resolvePath(filesBasePath, filePath), "utf-8");
-        const output = fileSrc.replace(/(\r\n|\n|\r)/gm, os.EOL);
+        const { mode, size } = fs.statSync(resolvePath(filesBasePath, filePath));
 
         if (!validateDirectoryExists(outputDirname)) {
             fs.mkdirSync(outputDirname, { recursive: true });
         }
 
+        if (isBinaryFileSync(resolvePath(filesBasePath, filePath), size)) {
+            fs.createReadStream(resolvePath(filesBasePath, filePath)).pipe(fs.createWriteStream(outputPath, {
+              mode,
+            }));
+            continue;
+        }
+
+        const fileSrc = fs.readFileSync(resolvePath(filesBasePath, filePath), "utf-8");
+        const output = fileSrc.replace(/(\r\n|\n|\r)/gm, os.EOL);
         fs.writeFileSync(outputPath, output, { encoding: "utf-8", flag: "w", mode });
     }
 }
