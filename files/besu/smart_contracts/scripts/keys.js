5fc57a0eab2d6b50d6270955f2dbeddb400ad5b3
Joshua Fernandes <joshua.fernandes@consensys.net>
2021-09-22 19:52:58 +1000

    commit1 - cleanup

--- files/besu/smart_contracts/scripts/keys.js
+++ files/besu/smart_contracts/scripts/keys.js
@@ -17,17 +17,17 @@ module.exports = {
     member1: {
       url: "http://127.0.0.1:20000",
       wsUrl: "ws://127.0.0.1:20001",
-      privateKey: "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"
+      privateKey: "b9a4bd1539c15bcc83fa9078fe89200b6e9e802ae992f13cd83c853f16e8bed4"
     },
     member2: {
       url: "http://127.0.0.1:20002",
       wsUrl: "ws://127.0.0.1:20003",
-      privateKey: "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"
+      privateKey: "f18166704e19b895c1e2698ebc82b4e007e6d2933f4b31be23662dd0ec602570"
     },
     member3: {
       url: "http://127.0.0.1:20004",
       wsUrl: "ws://127.0.0.1:20005",
-      privateKey: "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"
+      privateKey: "4107f0b6bf67a3bc679a15fe36f640415cf4da6a4820affaac89c8b280dfd1b3"
     },
     ethsignerProxy: {
       url: "http://127.0.0.1:18545",
