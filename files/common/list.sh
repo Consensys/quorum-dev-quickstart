bb2b30bfca2579e26f3d19c5c5fcd76f229944e6
Joshua Fernandes <joshua.fernandes@consensys.net>
2022-02-14 11:30:02 +1000

    fix the grafana link (#145)

--- files/common/list.sh
+++ files/common/list.sh
@@ -74,9 +74,9 @@ fi
 if [ ! -z `docker-compose -f docker-compose.yml ps -q prometheus 2> /dev/null` ]; then
 echo "Prometheus address                             : http://${HOST}:9090/graph"
 fi
-grafana_url="http://${HOST}:3000/d/XE4V0WGZz/besu-overview?orgId=1&refresh=10s&from=now-30m&to=now&var-system=All"
-if [ ! -z `docker ps -q --filter 'label=consensus=goquorum' 2> /dev/null ` ]; then
 grafana_url="http://${HOST}:3000/d/a1lVy7ycin9Yv/goquorum-overview?orgId=1&refresh=10s&from=now-30m&to=now&var-system=All"
+if [[ ! -z `docker ps -q --filter 'label=consensus=besu' 2> /dev/null ` ]]; then
+grafana_url="http://${HOST}:3000/d/XE4V0WGZz/besu-overview?orgId=1&refresh=10s&from=now-30m&to=now&var-system=All"
 fi
 if [ ! -z `docker-compose -f docker-compose.yml ps -q grafana 2> /dev/null` ]; then
 echo "Grafana address                                : $grafana_url"
