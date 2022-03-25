da231b30213f3cecb6461c8f3a9ee0bd6cd86236
Joshua Fernandes <joshua.fernandes@consensys.net>
2022-03-25 22:26:38 +1000

    adding ethersjs and updating accounts in genesis file (#156)
    * adding ethersjs and updating accounts in genesis file
    
    * pr fixes

--- files/goquorum/smart_contracts/scripts/keys.js
+++ files/goquorum/smart_contracts/scripts/keys.js
@@ -39,5 +39,16 @@ module.exports = {
       privateKey: "4107f0b6bf67a3bc679a15fe36f640415cf4da6a4820affaac89c8b280dfd1b3",
       accountAddress: "0fbdc686b912d7722dc86510934589e0aaf3b55a"
     }
-  }
+  },
+  accounts: {
+    "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73" : {
+      "privateKey" : "8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
+    },
+    "0x627306090abaB3A6e1400e9345bC60c78a8BEf57" : {
+      "privateKey" : "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
+      },
+    "0xf17f52151EbEF6C7334FAD080c5704D77216b732" : {
+      "privateKey" : "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f",
+      },
+    }
 };
--- files/goquorum/smart_contracts/scripts/public_tx.js
+++ files/goquorum/smart_contracts/scripts/public_tx.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
-const Tx = require('ethereumjs-tx');
 const Web3 = require('web3');
-
 
 // member1 details
 const { tessera, quorum } = require("./keys.js");
@@ -15,7 +13,6 @@ const contractJsonPath = path.resolve(__dirname, '../','contracts','SimpleStorag
 const contractJson = JSON.parse(fs.readFileSync(contractJsonPath));
 const contractAbi = contractJson.abi;
 const contractBytecode = contractJson.evm.bytecode.object
-
 // initialize the default constructor with a value `47 = 0x2F`; this value is appended to the bytecode
 const contractConstructorInit = "000000000000000000000000000000000000000000000000000000000000002F";
 
@@ -61,26 +58,24 @@ async function createContract(host) {
   const account = web3.eth.accounts.create();
   console.log(account);
 
-  const rawTxOptions = {
+  const txn = {
     chainId: 1337,
-    nonce: "0x00",
+    nonce: await web3.eth.getTransactionCount(account.address),       // 0x00 because this is a new account
     from: account.address,
-    to: null, //public tx
+    to: null,            //public tx
     value: "0x00",
     data: '0x'+contractBytecode+contractConstructorInit,
-    gasPrice: "0x0", //ETH per unit of gas
-    gasLimit: "0x2CA51" //max number of gas units the tx is allowed to use
+    gasPrice: "0x0",     //ETH per unit of gas
+    gas: "0x2CA51"  //max number of gas units the tx is allowed to use
   };
-  console.log("Creating transaction...");
-  const tx = new Tx(rawTxOptions);
-  console.log("Signing transaction...");
-  tx.sign(Buffer.from(account.privateKey.substring(2), "hex"));
-  console.log("Sending transaction...");
-  var serializedTx = tx.serialize();
-  const pTx = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex').toString("hex"));
-  console.log("tx transactionHash: " + pTx.transactionHash);
-  console.log("tx contractAddress: " + pTx.contractAddress);
-  return pTx;
+
+  console.log("create and sign the txn")
+  const signedTx = await web3.eth.accounts.signTransaction(txn, account.privateKey);
+  console.log("sending the txn")
+  const txReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  console.log("tx transactionHash: " + txReceipt.transactionHash);
+  console.log("tx contractAddress: " + txReceipt.contractAddress);
+  return txReceipt;
 };
 
 async function main(){
