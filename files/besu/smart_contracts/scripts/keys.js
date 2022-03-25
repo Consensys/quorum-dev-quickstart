5cfb8f9082bc9ffc299a94bce1c8fadeb360e64e
Joshua Fernandes <joshua.fernandes@consensys.net>
2022-03-25 22:23:33 +1000

    pr fixes

--- files/besu/smart_contracts/scripts/keys.js
+++ files/besu/smart_contracts/scripts/keys.js
@@ -41,7 +41,7 @@ module.exports = {
     "0x627306090abaB3A6e1400e9345bC60c78a8BEf57" : {
       "privateKey" : "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
       },
-    "f17f52151EbEF6C7334FAD080c5704D77216b732" : {
+    "0xf17f52151EbEF6C7334FAD080c5704D77216b732" : {
       "privateKey" : "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f",
       },
     }
