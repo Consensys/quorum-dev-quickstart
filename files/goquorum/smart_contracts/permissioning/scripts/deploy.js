ccfad18e85ca5eafba17e7bb9fd726af95aa909e
Eric Lin <eric.lin@consensys.net>
2022-05-06 15:54:40 +1000

    update web3js implementation with awaits and pass gas limit
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- files/goquorum/smart_contracts/permissioning/scripts/deploy.js
+++ files/goquorum/smart_contracts/permissioning/scripts/deploy.js
@@ -8,22 +8,11 @@ const host = quorum.member1.url;
 const guardianAddress = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
 
 let web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider("host"));
+web3.setProvider(new web3.providers.HttpProvider(host));
 
 const privateKey = quorum.member1.privateKey;
 const account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
 const accountAddress = account.address;
-
-const deployedContractAddresses = {
-  PermissionsUpgradable: "",
-  OrgManager: "",
-  RoleManager: "",
-  AccountManager: "",
-  VoterManager: "",
-  NodeManager: "",
-  PermissionsInterface: "",
-  PermissionsImplementation: "",
-};
 
 async function contractDeployer(contract, arguments) {
   const abi = JSON.parse(
@@ -37,56 +26,52 @@ async function contractDeployer(contract, arguments) {
     )
     .toString();
   let myContract = new web3.eth.Contract(abi);
-  await myContract
+  const res = await myContract
     .deploy({ data: bytecode, arguments: arguments })
     .send({
       from: accountAddress,
-      gas: 9200000,
-    })
-    .then(function (newContractInstance) {
-      console.log(
-        `${contract} Contract Address: ${newContractInstance.options.address}`
-      );
-      deployedContractAddresses[contract] = newContractInstance.options.address;
-      return newContractInstance.options.address;
+      // gas: 9200000,
     });
+  console.log(`${contract} Contract Address: ${res.options.address}`);
+  return res.options.address;
 }
 
-contractDeployer("PermissionsUpgradable", [guardianAddress]).then(() => {
-  contractDeployer("OrgManager", [
-    deployedContractAddresses["PermissionsUpgradable"],
-  ]).then(() => {
-    contractDeployer("RoleManager", [
-      deployedContractAddresses["PermissionsUpgradable"],
-    ]).then(() => {
-      contractDeployer("AccountManager", [
-        deployedContractAddresses["PermissionsUpgradable"],
-      ]).then(() => {
-        contractDeployer("VoterManager", [
-          deployedContractAddresses["PermissionsUpgradable"],
-        ]).then(() => {
-          contractDeployer("NodeManager", [
-            deployedContractAddresses["PermissionsUpgradable"],
-          ]).then(() => {
-            contractDeployer("PermissionsInterface", [
-              deployedContractAddresses["PermissionsUpgradable"],
-            ]).then(() => {
-              // had to change maxCodeSizeConfig in GoQ to 128kb
-              contractDeployer("PermissionsImplementation", [
-                deployedContractAddresses["PermissionsUpgradable"],
-                deployedContractAddresses["OrgManager"],
-                deployedContractAddresses["RoleManager"],
-                deployedContractAddresses["AccountManager"],
-                deployedContractAddresses["VoterManager"],
-                deployedContractAddresses["NodeManager"],
-              ]);
-            });
-          });
-        });
-      });
-    });
-  });
-});
+(async function main() {
+  const permissionUpgradableAddress = await contractDeployer(
+    "PermissionsUpgradable",
+    [guardianAddress]
+  );
+  const orgManagerAddress = await contractDeployer("OrgManager", [
+    permissionUpgradableAddress,
+  ]);
+  const roleManagerAddress = await contractDeployer("RoleManager", [
+    permissionUpgradableAddress,
+  ]);
+  const accountManagerAddress = await contractDeployer("AccountManager", [
+    permissionUpgradableAddress,
+  ]);
+  const voterManagerAddress = await contractDeployer("VoterManager", [
+    permissionUpgradableAddress,
+  ]);
+  const nodeManagerAddress = await contractDeployer("NodeManager", [
+    permissionUpgradableAddress,
+  ]);
+  const permissionsInterfaceAddress = await contractDeployer(
+    "PermissionsInterface",
+    [permissionUpgradableAddress]
+  );
+  const permissionsImplementationAddress = await contractDeployer(
+    "PermissionsImplementation",
+    [
+      permissionUpgradableAddress,
+      orgManagerAddress,
+      roleManagerAddress,
+      accountManagerAddress,
+      voterManagerAddress,
+      nodeManagerAddress,
+    ]
+  );
+})();
 
 // const upgr = new web3.eth.Contract(abi).at(address);
 // const tx = upgr.init(
