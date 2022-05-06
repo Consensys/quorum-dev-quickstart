190c67c9e51efe2a620240300024663f693844e6
Eric Lin <eric.lin@consensys.net>
2022-05-06 14:57:27 +1000

    test deploy contracts in order
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- files/goquorum/smart_contracts/permissioning/scripts/deploy.js
+++ files/goquorum/smart_contracts/permissioning/scripts/deploy.js
@@ -1,27 +1,100 @@
-const path = require('path');
-const fs = require('fs-extra');
-const Web3 = require('web3');
+const path = require("path");
+const fs = require("fs-extra");
+const Web3 = require("web3");
 
 // member1 details
 const { quorum } = require("./keys.js");
 const host = quorum.member1.url;
-const accountAddress = quorum.member1.accountAddress;
+const guardianAddress = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
 
-// ac = eth.accounts[0];
-// web3.eth.defaultAccount = ac;
-const abi = fs.readFileSync(path.resolve(__dirname, '../','contracts/v2/output','PermissionsUpgradable.abi'));
-const bytecode = fs.readFileSync(path.resolve(__dirname, '../','contracts/v2/output','PermissionsUpgradable.bin'));
+let web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider("host"));
 
-var simpleContract = web3.eth.contract(abi);
-var a = simpleContract.new("<guardian address>", {from:web3.eth.accounts[0], data: bytecode, gas: 9200000}, function(e, contract) {
-	if (e) {
-		console.log("err creating contract", e);
-	} else {
-		if (!contract.address) {
-			console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined ...");
-		} else {
-			console.log("Contract mined! Address: " + contract.address);
-			console.log(contract);
-		}
-	}
+const privateKey = quorum.member1.privateKey;
+const account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
+const accountAddress = account.address;
+
+const deployedContractAddresses = {
+  PermissionsUpgradable: "",
+  OrgManager: "",
+  RoleManager: "",
+  AccountManager: "",
+  VoterManager: "",
+  NodeManager: "",
+  PermissionsInterface: "",
+  PermissionsImplementation: "",
+};
+
+async function contractDeployer(contract, arguments) {
+  const abi = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, "../", "contracts/v2/output", `${contract}.abi`)
+    )
+  );
+  const bytecode = fs
+    .readFileSync(
+      path.resolve(__dirname, "../", "contracts/v2/output", `${contract}.bin`)
+    )
+    .toString();
+  let myContract = new web3.eth.Contract(abi);
+  await myContract
+    .deploy({ data: bytecode, arguments: arguments })
+    .send({
+      from: accountAddress,
+      gas: 9200000,
+    })
+    .then(function (newContractInstance) {
+      console.log(
+        `${contract} Contract Address: ${newContractInstance.options.address}`
+      );
+      deployedContractAddresses[contract] = newContractInstance.options.address;
+      return newContractInstance.options.address;
+    });
 }
+
+contractDeployer("PermissionsUpgradable", [guardianAddress]).then(() => {
+  contractDeployer("OrgManager", [
+    deployedContractAddresses["PermissionsUpgradable"],
+  ]).then(() => {
+    contractDeployer("RoleManager", [
+      deployedContractAddresses["PermissionsUpgradable"],
+    ]).then(() => {
+      contractDeployer("AccountManager", [
+        deployedContractAddresses["PermissionsUpgradable"],
+      ]).then(() => {
+        contractDeployer("VoterManager", [
+          deployedContractAddresses["PermissionsUpgradable"],
+        ]).then(() => {
+          contractDeployer("NodeManager", [
+            deployedContractAddresses["PermissionsUpgradable"],
+          ]).then(() => {
+            contractDeployer("PermissionsInterface", [
+              deployedContractAddresses["PermissionsUpgradable"],
+            ]).then(() => {
+              // had to change maxCodeSizeConfig in GoQ to 128kb
+              contractDeployer("PermissionsImplementation", [
+                deployedContractAddresses["PermissionsUpgradable"],
+                deployedContractAddresses["OrgManager"],
+                deployedContractAddresses["RoleManager"],
+                deployedContractAddresses["AccountManager"],
+                deployedContractAddresses["VoterManager"],
+                deployedContractAddresses["NodeManager"],
+              ]);
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+// const upgr = new web3.eth.Contract(abi).at(address);
+// const tx = upgr.init(
+//   deployedContractAddresses["PermissionsInterface"],
+//   deployedContractAddresses["PermissionsImplementation"],
+//   {
+//     from: account,
+//     gas: 4500000,
+//   }
+// );
+// console.log(`Init transaction id: ${tx}`);
