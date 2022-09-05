142d5439c0849fb7305487829499fc1b34b1c4a0
Joshua Fernandes <joshua.fernandes@consensys.net>
2022-09-06 07:09:33 +1000

    Loki (#202)
    * adding in loki
    
    * loki dashboard

--- files/common/list.sh
+++ files/common/list.sh
@@ -75,11 +75,14 @@ if [ ! -z `docker-compose -f docker-compose.yml ps -q prometheus 2> /dev/null` ]
 echo "Prometheus address                             : http://${HOST}:9090/graph"
 fi
 grafana_url="http://${HOST}:3000/d/a1lVy7ycin9Yv/goquorum-overview?orgId=1&refresh=10s&from=now-30m&to=now&var-system=All"
+grafana_loki_url="http://${HOST}:3000/d/Ak6eXLsPxFemKYKEXfcH/quorum-logs-loki?orgId=1&var-app=quorum&var-search="
 if [[ ! -z `docker ps -q --filter 'label=consensus=besu' 2> /dev/null ` ]]; then
 grafana_url="http://${HOST}:3000/d/XE4V0WGZz/besu-overview?orgId=1&refresh=10s&from=now-30m&to=now&var-system=All"
+grafana_loki_url="http://${HOST}:3000/d/Ak6eXLsPxFemKYKEXfcH/quorum-logs-loki?orgId=1&var-app=besu&var-search="
 fi
 if [ ! -z `docker-compose -f docker-compose.yml ps -q grafana 2> /dev/null` ]; then
 echo "Grafana address                                : $grafana_url"
+echo "Collated logs using Grafana and Loki           : $grafana_loki_url"
 fi
 
 if [ $elk_setup == true ]; then
