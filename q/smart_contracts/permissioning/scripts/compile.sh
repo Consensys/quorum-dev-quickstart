fbbc4cfa43decae886665f15abab74cd75eaf478
Eric Lin <eric.lin@consensys.net>
2023-08-29 15:45:15 +1000

    remove test folder
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- q/smart_contracts/permissioning/scripts/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ./contracts/v2
-for f in *.sol
-do
-	echo "Compiling $f contract..."
-	solc --bin --abi --optimize --overwrite -o ./output $f
-done
--- q/smart_contracts/permissioning/scripts/copyAndRestart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Copying permission-config.json file to the volume"
-cp permission-config.json ../../config/permissions/ 
-cd ../../ 
-echo "Restarting network stop"
-./restart.sh
